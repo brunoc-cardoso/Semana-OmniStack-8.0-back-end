9a798d2832c809745335166cfa7bb17615c1df1c
bruno <brunocpessoal@gmail.com>
2019-11-10 13:36:18 +0000

    added socket io in API

--- src/server.js
+++ src/server.js
@@ -4,18 +4,34 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const port = 3333;
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+  connectUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-le6dz.mongodb.net/omnistack08?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-server.use(express.json());
-server.use(cors());
-server.use(routes);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectUsers = connectUsers;
 
+  return next();
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 server.listen(port);
 
 console.log(`Application running on port ${port}`);
+
